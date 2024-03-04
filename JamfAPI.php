c970084ee1fdf055381ac5b621108ed3b4e3deb1
Beriyack <33500849+Beriyack@users.noreply.github.com>
2024-03-04 09:08:54 +0100

    Update JamfAPI.php

--- JamfAPI.php
+++ JamfAPI.php
@@ -5,19 +5,39 @@ class JamfAPI {
     private $base_url = "https://apiv6.zuludesk.com/";
     private $key;
     private $networkID;
+    private $options;
 
     public function __construct($networkID, $key) {
         $this->networkID = $networkID;
         $this->key = $key;
+        $this->options = $this->options();
     }
 
-    public function applications(): array {
-        $options = array(
+    private function options(): array {
+        return array(
             CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
             CURLOPT_USERPWD => $this->networkID . ":" . $this->key,
             CURLOPT_CAINFO => __DIR__ . "/Amazon Root CA 1.crt"
         );
+    }
 
-        return CurlHelper::get($this->base_url . "apps", $options);
+    private function getData(string $type) : array {
+        return CurlHelper::get($this->base_url . $type, $this->options);
+    }
+
+    public function applications(): array {
+        return $this->getData("apps");
+    }
+
+    public function classes(): array {
+        return $this->getData("classes");
+    }
+
+    public function devices(): array {
+        return $this->getData("devices");
+    }
+
+    public function devices_groups(): array {
+        return $this->getData("devices/groups");
     }
 }
